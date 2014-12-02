8c2a94c7670c9545e75aab191e0a24aa4cafca15
kawasima <kawasima1016@gmail.com>
2014-12-02 20:17:56 +0900

    init

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject net.unit8/remember-the-wallet "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [sablono "0.2.22"]
+                 [prismatic/om-tools "0.3.6"]
+                 [om "0.7.3"]])
---
+++ src/cljs/remember_the_wallet/core.cljs
@@ -0,0 +1,6 @@
+(ns remember-the-wallet.core
+  (:require [om.core :as om :include-macros true]
+            [om-tools.dom :as dom :include-macros true])
+  (:use [om-tools.core :only (defcomponent)])
+
+(defcomponent)
